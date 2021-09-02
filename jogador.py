d90ea1059c781cca5266dfc273f5c23253395911
C1310860 Augusto Lima dos Santos <c1310860@interno.bb.com.br>
2021-09-02 19:19:39 -0300

    Testanto o fluxo de compra e aluguel, novos metodos foram adicionados

--- jogador.py
+++ jogador.py
@@ -7,7 +7,7 @@ class Jogador():
     def __init__(self, identificacao):
         self.identificacao = identificacao
         self.saldo = 300
-        self.casa_atual = 1
+        self.casa_atual = 0
         self.rodada = 1
         self.ativo = True
     
@@ -15,7 +15,8 @@ class Jogador():
         return "Identificacao: "+str(self.identificacao) \
                 +" Status: "+str(self.ativo) \
                 +" Saldo: "+str(self.saldo) \
-                +" Casa Atual: "+str(self.casa_atual)
+                +" Casa Atual: "+str(self.casa_atual) \
+                +" Rodada: "+str(self.rodada) 
 
     def continuar_jogando(self):
         if self.ativo:
@@ -39,18 +40,31 @@ class Jogador():
     def comprar_propriedade(self, valor):
         if valor <= self.saldo:
             self.saldo -= valor
+            return True
+        return False
 
-    def acao_jogador(self):
-        pass
-
-    def atualiza_casa_do_jogo(self):
-        self.casa_atual += self.jogar_dado
-        if self.casa_atual > MAXIMO_CASAS:
-            self.saldo += 100
-            self.casa_atual = 1 # MAXIMO_CASAS - self.casa_atual | Outra possibilidade para o início da próxima rodada
-            self.rodada += 1
+    def acao_jogador(self, propriedade):
+        acao = "nada"
+        if propriedade.disponivel_para_compra():
+            if self.comprar_propriedade(propriedade.get_custo_venda()):
+                propriedade.novo_proprietario(self)
+                acao = "comprou"
         else:
-            self.acao_jogador()
+            acao = "alugou"
+            valor_recebido = propriedade.get_valor_aluguel()
+            self.pagar_aluguel(valor_recebido)
+            propriedade.cobrar_aluguel(valor_recebido)
+            
+        print("++++++++++++", propriedade, acao)
+
+    def realizar_jogada(self, propriedades):
+        self.casa_atual += self.jogar_dado()
+        if self.casa_atual <= MAXIMO_CASAS:
+            self.acao_jogador(propriedades[self.casa_atual-1])
+        else:
+            self.saldo += 100
+            self.casa_atual = self.casa_atual - MAXIMO_CASAS # Outra possibilidade é voltar para 0 ou 1
+            self.rodada += 1
     
     
 
--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from jogador import Jogador
+from propriedade import Propriedade
 
 
 def execucao_simples():
-    jog1 = Jogador("1")
-    jog2 = Jogador("2")
-    jog3 = Jogador("3")
-    jog4 = Jogador("4")
-    jogadores = [jog1, jog2, jog3, jog4]
+    propriedades = [Propriedade(1, 50, 10), Propriedade(2, 70, 15), \
+                    Propriedade(3, 150, 40), Propriedade(4, 100, 20), \
+                    Propriedade(5, 80, 30), Propriedade(6, 120, 40), \
+                    Propriedade(7, 100, 25), Propriedade(8, 170, 50), \
+                    Propriedade(9, 40, 5), Propriedade(10, 140, 60), \
+                    Propriedade(11, 210, 120), Propriedade(12, 200, 100), \
+                    Propriedade(13, 220, 100), Propriedade(14, 190, 90), \
+                    Propriedade(15, 300, 10), Propriedade(16, 270, 170), \
+                    Propriedade(17, 200, 130), Propriedade(18, 160, 75), \
+                    Propriedade(19, 50, 10), Propriedade(20, 235, 100)]
+    
+    jogadores = [Jogador("João"), Jogador("Maria"), Jogador("Bruna"), Jogador("Tales")]
 
-    for jog in jogadores:
-        if jog.continuar_jogando():
-            print(jog)
+    for simulacao in range(5):
+        for jog in jogadores:
+            if jog.continuar_jogando():
+                jog.realizar_jogada(propriedades)
+                print("JOGADOR:", jog)
+        print("----------------------------------------")
+    
 
 
 if __name__ == "__main__":
