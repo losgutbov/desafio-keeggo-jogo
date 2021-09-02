c72ab3880c32e70a9a764b2935b1901c2cabc5b4
C1310860 Augusto Lima dos Santos <c1310860@interno.bb.com.br>
2021-09-02 16:35:08 -0300

    Iniciando as classes de jogadores e propriedades

---
+++ jogador.py
@@ -0,0 +1,12 @@
+import random
+import definicoes
+
+
+class Jogador():
+    
+    def __init__(self):
+        self.saldo = 300
+        self.casa_atual = 1
+        self.rodada = 1
+    
+   
---
+++ propriedade.py
@@ -0,0 +1,9 @@
+
+
+class Propriedade():
+    
+    def __init__(self):
+        self.custo_venda = 0
+        self.valor_aluguel = 0
+        self.proprietario = None
+        
